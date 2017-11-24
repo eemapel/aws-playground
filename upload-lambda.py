b0653b0bc049eb8fc7e2e188a7761df10f287ca9
Marko Pellikka <eemapel@gmail.com>
2017-11-24 13:52:17 -0800

    SNS notification added

--- upload-lambda.py
+++ upload-lambda.py
@@ -4,21 +4,30 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:185831608739:deployLambdaTopic')
 
-    s3 = boto3.resource('s3')
+    try:
+        s3 = boto3.resource('s3')
 
-    lambda_bucket = s3.Bucket('lambda.pellikka.net')
-    build_bucket = s3.Bucket('lambdabuild.pellikka.net')
+        lambda_bucket = s3.Bucket('lambda.pellikka.net')
+        build_bucket = s3.Bucket('lambdabuild.pellikka.net')
 
-    lambda_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('lambdabuild.zip', lambda_zip)
+        lambda_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('lambdabuild.zip', lambda_zip)
 
-    with zipfile.ZipFile(lambda_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            lambda_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={ 'ContentType': mimetypes.guess_type(nm)[0] })
-            lambda_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(lambda_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                lambda_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={ 'ContentType': mimetypes.guess_type(nm)[0] })
+                lambda_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    print "Job Done!"
+        print "Job Done!"
+        topic.publish(Subject="Deployed", Message="Deployed successfully!")
+
+    except:
+        topic.publish(Subject="Deploy failed", Message="Deployment failed!")
+        raise
+
     return 'Hello from Lambda'
