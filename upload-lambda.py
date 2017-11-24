71dc6a5937acbbcf77ed8cd4e5ba6156abdd16a5
Marko Pellikka <eemapel@gmail.com>
2017-11-24 13:06:01 -0800

    Modified lambda function to work in AWS

--- upload-lambda.py
+++ upload-lambda.py
@@ -1,22 +1,24 @@
-#!/usr/bin/env python
-
 import boto3
 import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-lambda_bucket = s3.Bucket('lambda.pellikka.net')
-build_bucket = s3.Bucket('lambdabuild.pellikka.net')
+    s3 = boto3.resource('s3')
 
-lambda_zip = StringIO.StringIO()
-build_bucket.download_fileobj('lambdabuild.zip', lambda_zip)
+    lambda_bucket = s3.Bucket('lambda.pellikka.net')
+    build_bucket = s3.Bucket('lambdabuild.pellikka.net')
 
-with zipfile.ZipFile(lambda_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        lambda_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={ 'ContentType': mimetypes.guess_type(nm)[0] })
-        lambda_bucket.Object(nm).Acl().put(ACL='public-read')
+    lambda_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('lambdabuild.zip', lambda_zip)
 
+    with zipfile.ZipFile(lambda_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            lambda_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={ 'ContentType': mimetypes.guess_type(nm)[0] })
+            lambda_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job Done!"
+    return 'Hello from Lambda'
