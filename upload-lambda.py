5a52b45b290727ae0b0ff932c7080160c8a8642b
Marko Pellikka <eemapel@gmail.com>
2017-11-24 12:25:50 -0800

    Added lambda function for uploading code

---
+++ upload-lambda.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+lambda_bucket = s3.Bucket('lambda.pellikka.net')
+build_bucket = s3.Bucket('lambdabuild.pellikka.net')
+
+lambda_zip = StringIO.StringIO()
+build_bucket.download_fileobj('lambdabuild.zip', lambda_zip)
+
+with zipfile.ZipFile(lambda_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        lambda_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={ 'ContentType': mimetypes.guess_type(nm)[0] })
+        lambda_bucket.Object(nm).Acl().put(ACL='public-read')
+
